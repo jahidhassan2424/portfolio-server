2098427d7dc9d53ee6aaef46f5b4021337ffdded
Jahid Hassan <jahidhassan.programmer@gmail.com>
2023-04-18 00:00:17 -0300

    demo comment added after updating environment variable

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ async function run() {
             const toEmail = body.toEmail;
             const subject = body.subject;
             const text = body.text;
-
+            // 
             const transporter = nodemailer.createTransport({
                 service: "hotmail",
                 auth: {
