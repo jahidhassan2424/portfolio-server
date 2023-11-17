ad94a7940941fa5db0280ba55ea8d26306bd4c90
Jahid Hassan <jh210293@dal.ca>
2023-11-17 17:31:01 -0400

    DB changed

--- db/index.js
+++ db/index.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
-mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jr5hf0s.mongodb.net/?retryWrites=true&w=majority`)
-    .then(() => console.log("DB Connected"))
-    .catch((err) => console.log(err));
+// const mongoose = require('mongoose');
+// mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jr5hf0s.mongodb.net/?retryWrites=true&w=majority`) // Portfolio 
+//     .then(() => console.log("DB Connected"))
+//     .catch((err) => console.log(err));
