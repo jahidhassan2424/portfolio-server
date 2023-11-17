a7ef3487b94080f10242d163e5a2a2be10dfa5b0
Jahid Hassan <jh210293@dal.ca>
2023-11-17 18:11:56 -0400

    try catch does not cover any api

--- index.js
+++ index.js
@@ -17,57 +17,6 @@ try {
     async function run() {
         await client.connect();
         console.log('DB Connected');
-        const projectsCollection = client.db("portfolio").collection("projects");
-
-        //get requests
-
-        //Get all projects
-        app.get('/projects', async (req, res) => {
-            const data = await projectsCollection.find().toArray();
-            res.send(data);
-        });
-        app.get('/projects/:id', async (req, res) => {
-            const { id } = req.params;
-            const data = await projectsCollection.find({ _id: ObjectId(id) }).toArray();
-            res.send(data);
-        });
-        app.post('/email', async (req, res) => {
-
-            const body = req.body;
-            const toEmail = body.toEmail;
-            const subject = body.subject;
-            const text = body.text;
-            // 
-            const transporter = nodemailer.createTransport({
-                service: "hotmail",
-                auth: {
-
-                    user: process.env.sendFromEmail,
-                    pass: process.env.passSendFrom,
-                },
-                from: process.env.sendFromEmail,
-
-            });
-            const options = {
-                from: req.body.sendFromEmail,
-                to: `${toEmail}`,
-                subject: `${subject}`,
-                text: `${text}`
-            }
-            transporter.sendMail(options, function (err, info) {
-                if (err) {
-                    console.log(err);
-                    res.send({ success: false })
-                }
-                else {
-                    res.send({ success: true })
-                }
-            })
-        });
-
-
-
-
 
     }
     run().catch(console.dir);
@@ -75,6 +24,59 @@ try {
     console.log(error);
 
 }
+
+const projectsCollection = client.db("portfolio").collection("projects");
+
+//get requests
+
+//Get all projects
+app.get('/projects', async (req, res) => {
+    const data = await projectsCollection.find().toArray();
+    res.send(data);
+});
+app.get('/projects/:id', async (req, res) => {
+    const { id } = req.params;
+    const data = await projectsCollection.find({ _id: ObjectId(id) }).toArray();
+    res.send(data);
+});
+app.post('/email', async (req, res) => {
+
+    const body = req.body;
+    const toEmail = body.toEmail;
+    const subject = body.subject;
+    const text = body.text;
+    // 
+    const transporter = nodemailer.createTransport({
+        service: "hotmail",
+        auth: {
+
+            user: process.env.sendFromEmail,
+            pass: process.env.passSendFrom,
+        },
+        from: process.env.sendFromEmail,
+
+    });
+    const options = {
+        from: req.body.sendFromEmail,
+        to: `${toEmail}`,
+        subject: `${subject}`,
+        text: `${text}`
+    }
+    transporter.sendMail(options, function (err, info) {
+        if (err) {
+            console.log(err);
+            res.send({ success: false })
+        }
+        else {
+            res.send({ success: true })
+        }
+    })
+});
+
+
+
+
+
 
 app.get('/', async (req, res) => {
     res.send("Server Running")
