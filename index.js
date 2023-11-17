9ebf50c1d25b9419e409e193f420c71c39bc7469
Jahid Hassan <jh210293@dal.ca>
2023-11-17 17:32:00 -0400

    db Changed

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ const nodemailer = require('nodemailer');
 app.use(cors())
 app.use(express.json())
 //
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.h2ts2.mongodb.net/?retryWrites=true&w=majority`;
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.h2ts2.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jr5hf0s.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run() {
     try {
