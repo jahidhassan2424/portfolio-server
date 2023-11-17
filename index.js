7b7ec911ab47c808c31cd1181e4ded17c365ceb6
Jahid Hassan <jh210293@dal.ca>
2023-11-17 17:59:41 -0400

    database changed to old one or Nissan parts database

--- index.js
+++ index.js
@@ -10,14 +10,14 @@ const nodemailer = require('nodemailer');
 app.use(cors())
 app.use(express.json())
 //
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.h2ts2.mongodb.net/?retryWrites=true&w=majority`;
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jr5hf0s.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.h2ts2.mongodb.net/?retryWrites=true&w=majority`;  // Nissan parts database
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jr5hf0s.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run() {
     try {
         await client.connect();
         console.log('DB Connected');
-        const projectsCollection = client.db("Portfolio").collection("projects");
+        const projectsCollection = client.db("portfolio").collection("projects");
 
         //get requests
 
